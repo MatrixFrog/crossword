67d4441885d0867311bc662753d958dd8dc49f31
Josh McKinney <joshka@users.noreply.github.com>
2025-07-08 23:29:10 -0700

    cargo fmt with default rust formatting options

--- crossword/src/checksum.rs
+++ crossword/src/checksum.rs
@@ -3,31 +3,31 @@
 
 #[must_use]
 pub(crate) fn checksum_region(base: &[u8], input_checksum: u16) -> u16 {
-  let mut checksum = input_checksum;
-  for &byte in base {
-    if checksum & 0x0001_u16 != 0 {
-      checksum = (checksum >> 1) + 0x8000
-    } else {
-      checksum = checksum >> 1;
+    let mut checksum = input_checksum;
+    for &byte in base {
+        if checksum & 0x0001_u16 != 0 {
+            checksum = (checksum >> 1) + 0x8000
+        } else {
+            checksum = checksum >> 1;
+        }
+        checksum = checksum.overflowing_add(byte as u16).0;
     }
-    checksum = checksum.overflowing_add(byte as u16).0;
-  }
-  checksum
+    checksum
 }
 
 /// For metadata (title, author, copyright, or notes), we do nothing if the string is
 /// empty, but if it's not empty we include the \0 byte in the calculation.
 #[must_use]
 pub(crate) fn checksum_metadata_string(s: &[u8], input_checksum: u16) -> u16 {
-  if s == b"\0" {
-    return input_checksum;
-  }
+    if s == b"\0" {
+        return input_checksum;
+    }
 
-  checksum_region(s, input_checksum)
+    checksum_region(s, input_checksum)
 }
 
 /// For clues, we do not include the trailing \0 byte.
 #[must_use]
 pub(crate) fn checksum_clue(s: &[u8], input_checksum: u16) -> u16 {
-  checksum_region(&s[0..s.len() - 1], input_checksum)
+    checksum_region(&s[0..s.len() - 1], input_checksum)
 }
